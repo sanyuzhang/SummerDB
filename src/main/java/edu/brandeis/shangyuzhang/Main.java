cfc5f81a410e4cb78de70a99f9f5711c78344ed0
SanyuZhang <deviltrigger1120@gmail.com>
2019-04-20 00:27:02 -0400

    Update Main.java

--- src/main/java/edu/brandeis/shangyuzhang/Main.java
+++ src/main/java/edu/brandeis/shangyuzhang/Main.java
@@ -23,7 +23,7 @@ public class Main {
     private static final boolean isLocal = false;
 
     private static TestDataSet testDataSet;
-    private static final String TARGET_DATA_SET = Constants.MEDIUM;
+    private static final String TARGET_DATA_SET = Constants.XSMALL;
 
     private static void load(String filePaths) {
         database.setLargeDataset(!filePaths.contains("data/x")
