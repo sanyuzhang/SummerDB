23c8637e2a6d636360292253e206f04fbe072fcd
SanyuZhang <deviltrigger1120@gmail.com>
2019-04-19 21:44:43 -0400

    Removed useless functions

--- src/main/java/edu/brandeis/shangyuzhang/operator/BaseJoin.java
+++ src/main/java/edu/brandeis/shangyuzhang/operator/BaseJoin.java
@@ -13,23 +13,23 @@ public abstract class BaseJoin implements RowsCounter {
 
     protected int numRows;
 
-    protected Iterator leftTable;
-    protected Iterator rightTable;
+    private Iterator leftTable;
+    private Iterator rightTable;
 
-    protected String firstTableName;
-    protected FilterPredicate firstTableFilterPredicate;
-    protected int firstTableNumOfRows;
+    private String firstTableName;
+    private FilterPredicate firstTableFilterPredicate;
+    private int firstTableNumOfRows;
 
-    protected String currTableName;
-    protected FilterPredicate currTableFilterPredicate;
-    protected int currTableNumOfRows;
+    private String currTableName;
+    private FilterPredicate currTableFilterPredicate;
+    private int currTableNumOfRows;
 
-    protected List<ParseElem[]> naturalJoinPairs;
-    protected Map<String, Integer> tableStartIndexMap;
+    private List<ParseElem[]> naturalJoinPairs;
+    private Map<String, Integer> tableStartIndexMap;
 
     protected boolean isCartesianJoin;
 
-    protected static final int BUFFER_SIZE = 1024 * 32;
+    private static final int BUFFER_SIZE = 1024 * 32;
     protected Database database = Database.getInstance();
 
     public BaseJoin(Iterator<int[]> leftIterator, Iterator<int[]> rightIterator, Map<String, Integer> startIndexMap,
@@ -82,6 +82,7 @@ public abstract class BaseJoin implements RowsCounter {
     }
 
     protected void naturalJoin() throws IOException {
+        List<Integer> leftNewCols = new ArrayList();
         List<Integer> rightNewCols = new ArrayList();
         NaturalJoinPredicate[] naturalJoinPredicates = new NaturalJoinPredicate[naturalJoinPairs.size()];
         for (int i = 0; i < naturalJoinPairs.size(); i++) {
@@ -89,6 +90,7 @@ public abstract class BaseJoin implements RowsCounter {
             int leftCol = translateColByTableName(leftElem.table, leftElem.col);
             int rightCol = database.getRelationByName(rightElem.table).getNewByOldCol(rightElem.col);
             naturalJoinPredicates[i] = new NaturalJoinPredicate(leftCol, rightCol);
+            leftNewCols.add(leftCol);
             rightNewCols.add(rightCol);
         }
 
@@ -96,44 +98,89 @@ public abstract class BaseJoin implements RowsCounter {
         Map<Integer, Map<Integer, Set<Integer>>> bufferHash = new HashMap(); // k: col, v: map:{ k: colValue, v: list of row_numbers}
         int pointer = 0;
 
-        while (true) {
-            boolean isRightEnd = true;
-            while (rightTable.hasNext()) {
-                bufferRows[pointer] = (int[]) rightTable.next();
-                for (int i = 0; i < rightNewCols.size(); i++) {
-                    int rightCol = rightNewCols.get(i);
-                    int rightColValue = bufferRows[pointer][rightCol];
-                    bufferHash.putIfAbsent(rightCol, new HashMap());
-                    bufferHash.get(rightCol).putIfAbsent(rightColValue, new HashSet());
-                    bufferHash.get(rightCol).get(rightColValue).add(pointer);
-                }
-                pointer++;
-                if (pointer < BUFFER_SIZE) continue;
-                isRightEnd = false;
-                break;
-            }
+        int leftSize = ((RowsCounter) leftTable).getNumOfRows();
+        int rightSize = ((RowsCounter) rightTable).getNumOfRows();
 
-            while (leftTable.hasNext()) {
-                int[] leftRow = (int[]) leftTable.next();
-
-                Set<Integer> rowsInBuffer = new HashSet();
-                for (int i = 0; i < naturalJoinPredicates.length; i++) {
-                    if (i > 0 && rowsInBuffer.size() == 0) break;
-                    NaturalJoinPredicate predicate = naturalJoinPredicates[i];
-                    int rightCol = predicate.rightCol, leftColValue = leftRow[predicate.leftCol];
-                    if (bufferHash.containsKey(rightCol) && bufferHash.get(rightCol).containsKey(leftColValue)) {
-                        if (i == 0) rowsInBuffer = new HashSet(bufferHash.get(rightCol).get(leftColValue));
-                        else rowsInBuffer.retainAll(bufferHash.get(rightCol).get(leftColValue));
-                    } else if (i > 0) {
-                        rowsInBuffer.clear();
+        if (!database.isLargeDataset() || leftSize >= rightSize) {
+            while (true) {
+                boolean isRightEnd = true;
+                while (rightTable.hasNext()) {
+                    bufferRows[pointer] = (int[]) rightTable.next();
+                    for (int i = 0; i < rightNewCols.size(); i++) {
+                        int rightCol = rightNewCols.get(i);
+                        int rightColValue = bufferRows[pointer][rightCol];
+                        bufferHash.putIfAbsent(rightCol, new HashMap());
+                        bufferHash.get(rightCol).putIfAbsent(rightColValue, new HashSet());
+                        bufferHash.get(rightCol).get(rightColValue).add(pointer);
                     }
+                    pointer++;
+                    if (pointer < BUFFER_SIZE) continue;
+                    isRightEnd = false;
+                    break;
                 }
-                for (int rowNum : rowsInBuffer) addToResultRows(leftRow, bufferRows[rowNum]);
+
+                while (leftTable.hasNext()) {
+                    int[] leftRow = (int[]) leftTable.next();
+
+                    Set<Integer> rowsInBuffer = new HashSet();
+                    for (int i = 0; i < naturalJoinPredicates.length; i++) {
+                        if (i > 0 && rowsInBuffer.size() == 0) break;
+                        NaturalJoinPredicate predicate = naturalJoinPredicates[i];
+                        int rightCol = predicate.rightCol, leftColValue = leftRow[predicate.leftCol];
+                        if (bufferHash.containsKey(rightCol) && bufferHash.get(rightCol).containsKey(leftColValue)) {
+                            if (i == 0) rowsInBuffer = new HashSet(bufferHash.get(rightCol).get(leftColValue));
+                            else rowsInBuffer.retainAll(bufferHash.get(rightCol).get(leftColValue));
+                        } else if (i > 0) {
+                            rowsInBuffer.clear();
+                        }
+                    }
+                    for (int rowNum : rowsInBuffer) addToResultRows(leftRow, bufferRows[rowNum]);
+                }
+                bufferHash.clear();
+                pointer = 0;
+                if (isRightEnd) break;
+                resetLeftIterator();
             }
-            bufferHash.clear();
-            pointer = 0;
-            if (isRightEnd) break;
-            resetLeftIterator();
+        } else {
+            while (true) {
+                boolean isLeftEnd = true;
+                while (leftTable.hasNext()) {
+                    bufferRows[pointer] = (int[]) leftTable.next();
+                    for (int i = 0; i < leftNewCols.size(); i++) {
+                        int leftCol = leftNewCols.get(i);
+                        int leftColValue = bufferRows[pointer][leftCol];
+                        bufferHash.putIfAbsent(leftCol, new HashMap());
+                        bufferHash.get(leftCol).putIfAbsent(leftColValue, new HashSet());
+                        bufferHash.get(leftCol).get(leftColValue).add(pointer);
+                    }
+                    pointer++;
+                    if (pointer < BUFFER_SIZE) continue;
+                    isLeftEnd = false;
+                    break;
+                }
+
+                while (rightTable.hasNext()) {
+                    int[] rightRow = (int[]) rightTable.next();
+
+                    Set<Integer> rowsInBuffer = new HashSet();
+                    for (int i = 0; i < naturalJoinPredicates.length; i++) {
+                        if (i > 0 && rowsInBuffer.size() == 0) break;
+                        NaturalJoinPredicate predicate = naturalJoinPredicates[i];
+                        int leftCol = predicate.leftCol, rightColValue = rightRow[predicate.rightCol];
+                        if (bufferHash.containsKey(leftCol) && bufferHash.get(leftCol).containsKey(rightColValue)) {
+                            if (i == 0) rowsInBuffer = new HashSet(bufferHash.get(leftCol).get(rightColValue));
+                            else rowsInBuffer.retainAll(bufferHash.get(leftCol).get(rightColValue));
+                        } else if (i > 0) {
+                            rowsInBuffer.clear();
+                        }
+                    }
+                    for (int rowNum : rowsInBuffer) addToResultRows(bufferRows[rowNum], rightRow);
+                }
+                bufferHash.clear();
+                pointer = 0;
+                if (isLeftEnd) break;
+                resetRightIterator();
+            }
         }
     }
 
--- src/main/java/edu/brandeis/shangyuzhang/operator/DiskSum.java
+++ src/main/java/edu/brandeis/shangyuzhang/operator/DiskSum.java
@@ -1,8 +1,6 @@
 package edu.brandeis.shangyuzhang.operator;
 
-import edu.brandeis.shangyuzhang.interfaces.RowsCounter;
 import edu.brandeis.shangyuzhang.model.FilterPredicate;
-import edu.brandeis.shangyuzhang.model.NaturalJoinPredicate;
 import edu.brandeis.shangyuzhang.model.ParseElem;
 
 import java.io.IOException;
@@ -35,109 +33,6 @@ public class DiskSum extends BaseJoin {
     protected void toJoinTable() throws IOException {
         if (!isCartesianJoin) naturalJoin();
         else cartesianJoin();
-    }
-
-    protected void naturalJoin() throws IOException {
-        List<Integer> leftNewCols = new ArrayList();
-        List<Integer> rightNewCols = new ArrayList();
-        NaturalJoinPredicate[] naturalJoinPredicates = new NaturalJoinPredicate[naturalJoinPairs.size()];
-        for (int i = 0; i < naturalJoinPairs.size(); i++) {
-            ParseElem leftElem = naturalJoinPairs.get(i)[0], rightElem = naturalJoinPairs.get(i)[1];
-            int leftCol = translateColByTableName(leftElem.table, leftElem.col);
-            int rightCol = database.getRelationByName(rightElem.table).getNewByOldCol(rightElem.col);
-            naturalJoinPredicates[i] = new NaturalJoinPredicate(leftCol, rightCol);
-            leftNewCols.add(leftCol);
-            rightNewCols.add(rightCol);
-        }
-
-        int[][] bufferRows = new int[BUFFER_SIZE][];
-        Map<Integer, Map<Integer, Set<Integer>>> bufferHash = new HashMap(); // k: col, v: map:{ k: colValue, v: list of row_numbers}
-        int pointer = 0;
-
-        int leftSize = ((RowsCounter) leftTable).getNumOfRows();
-        int rightSize = ((RowsCounter) rightTable).getNumOfRows();
-
-        if (leftSize >= rightSize) {
-            while (true) {
-                boolean isRightEnd = true;
-                while (rightTable.hasNext()) {
-                    bufferRows[pointer] = (int[]) rightTable.next();
-                    for (int i = 0; i < rightNewCols.size(); i++) {
-                        int rightCol = rightNewCols.get(i);
-                        int rightColValue = bufferRows[pointer][rightCol];
-                        bufferHash.putIfAbsent(rightCol, new HashMap());
-                        bufferHash.get(rightCol).putIfAbsent(rightColValue, new HashSet());
-                        bufferHash.get(rightCol).get(rightColValue).add(pointer);
-                    }
-                    pointer++;
-                    if (pointer < BUFFER_SIZE) continue;
-                    isRightEnd = false;
-                    break;
-                }
-
-                while (leftTable.hasNext()) {
-                    int[] leftRow = (int[]) leftTable.next();
-
-                    Set<Integer> rowsInBuffer = new HashSet();
-                    for (int i = 0; i < naturalJoinPredicates.length; i++) {
-                        if (i > 0 && rowsInBuffer.size() == 0) break;
-                        NaturalJoinPredicate predicate = naturalJoinPredicates[i];
-                        int rightCol = predicate.rightCol, leftColValue = leftRow[predicate.leftCol];
-                        if (bufferHash.containsKey(rightCol) && bufferHash.get(rightCol).containsKey(leftColValue)) {
-                            if (i == 0) rowsInBuffer = new HashSet(bufferHash.get(rightCol).get(leftColValue));
-                            else rowsInBuffer.retainAll(bufferHash.get(rightCol).get(leftColValue));
-                        } else if (i > 0) {
-                            rowsInBuffer.clear();
-                        }
-                    }
-                    for (int rowNum : rowsInBuffer) addToResultRows(leftRow, bufferRows[rowNum]);
-                }
-                bufferHash.clear();
-                pointer = 0;
-                if (isRightEnd) break;
-                resetLeftIterator();
-            }
-        } else {
-            while (true) {
-                boolean isLeftEnd = true;
-                while (leftTable.hasNext()) {
-                    bufferRows[pointer] = (int[]) leftTable.next();
-                    for (int i = 0; i < leftNewCols.size(); i++) {
-                        int leftCol = leftNewCols.get(i);
-                        int leftColValue = bufferRows[pointer][leftCol];
-                        bufferHash.putIfAbsent(leftCol, new HashMap());
-                        bufferHash.get(leftCol).putIfAbsent(leftColValue, new HashSet());
-                        bufferHash.get(leftCol).get(leftColValue).add(pointer);
-                    }
-                    pointer++;
-                    if (pointer < BUFFER_SIZE) continue;
-                    isLeftEnd = false;
-                    break;
-                }
-
-                while (rightTable.hasNext()) {
-                    int[] rightRow = (int[]) rightTable.next();
-
-                    Set<Integer> rowsInBuffer = new HashSet();
-                    for (int i = 0; i < naturalJoinPredicates.length; i++) {
-                        if (i > 0 && rowsInBuffer.size() == 0) break;
-                        NaturalJoinPredicate predicate = naturalJoinPredicates[i];
-                        int leftCol = predicate.leftCol, rightColValue = rightRow[predicate.rightCol];
-                        if (bufferHash.containsKey(leftCol) && bufferHash.get(leftCol).containsKey(rightColValue)) {
-                            if (i == 0) rowsInBuffer = new HashSet(bufferHash.get(leftCol).get(rightColValue));
-                            else rowsInBuffer.retainAll(bufferHash.get(leftCol).get(rightColValue));
-                        } else if (i > 0) {
-                            rowsInBuffer.clear();
-                        }
-                    }
-                    for (int rowNum : rowsInBuffer) addToResultRows(bufferRows[rowNum], rightRow);
-                }
-                bufferHash.clear();
-                pointer = 0;
-                if (isLeftEnd) break;
-                resetRightIterator();
-            }
-        }
     }
 
     protected void addToResultRows(int[] leftRow, int[] rightRow) {
